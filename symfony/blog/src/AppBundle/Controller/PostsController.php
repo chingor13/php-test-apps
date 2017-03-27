63f0c022695f8e73f9e86944c2b4c5af6553260d
Jeff Ching <chingor@google.com>
2017-03-27 13:25:19 -0700

    show blog posts

---
+++ symfony/blog/src/AppBundle/Controller/PostsController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class PostsController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction()
+    {
+        $repository = $this->getDoctrine()->getRepository('AppBundle:Posts');
+        $posts = $repository->findAll();
+
+        return $this->render('posts/index.html.twig', [
+            'posts' => $posts
+        ]);
+    }
+
+    /**
+     * @Route("/posts/{permalink}", name="posts.show")
+     */
+    public function showAction($permalink)
+    {
+        $repository = $this->getDoctrine()->getRepository('AppBundle:Posts');
+        $post = $repository->findOneByPermalink($permalink);
+        return $this->render('posts/show.html.twig', [
+            'post' => $post
+        ]);
+    }
+}
