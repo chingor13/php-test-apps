36ed958ff9a7c005abfa2dde6187ebed7c6d4821
Jeff Ching <chingor@google.com>
2017-03-27 13:26:54 -0700

    Add primary key

---
+++ laravel/blog/database/migrations/2017_03_27_193956_add_password_resets_primary_key.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPasswordResetsPrimaryKey extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('password_resets', function($table) {
+            $table->increments('id')->first();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('password_resets', function($table) {
+            $table->dropColumn('id');
+        });
+        //
+    }
+}
