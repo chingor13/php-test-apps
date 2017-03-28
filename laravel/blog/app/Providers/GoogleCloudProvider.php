ecabf18f855ae8ee93c12f57b2c9b0ea2d7690b7
Jeff Ching <chingor@google.com>
2017-03-28 10:20:22 -0700

    Add CacheItemPoolInterface Provider

--- laravel/blog/app/Providers/GoogleCloudProvider.php
+++ laravel/blog/app/Providers/GoogleCloudProvider.php
@@ -4,9 +4,12 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Google\Cloud\ServiceBuilder;
+use Google\Cloud\Trace\TraceClient;
 use Google\Cloud\Trace\RequestTracer;
 use Google\Cloud\Trace\Reporter\EchoReporter;
 use Google\Cloud\Trace\Reporter\TraceReporter;
+use Psr\Cache\CacheItemPoolInterface;
+use Madewithlove\IlluminatePsrCacheBridge\Laravel\CacheItem;
 
 class GoogleCloudProvider extends ServiceProvider
 {
@@ -15,37 +18,39 @@ class GoogleCloudProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot(ServiceBuilder $builder)
+    public function boot(TraceClient $trace, CacheItemPoolInterface $cache)
     {
-        $trace = $builder->trace();
-        $reporter = new TraceReporter($trace);
-        // $reporter = new EchoReporter($trace);
-
         // don't trace if we're running in the console (i.e. a php artisan command)
-        if (php_sapi_name() != 'cli') {
-            RequestTracer::start($trace, $reporter, [
-                'enabled' => true,
-                'startTime' => LARAVEL_START
-            ]);
+        if (php_sapi_name() == 'cli') {
+            return;
+        }
+
+        $reporter = new TraceReporter($trace);
+        RequestTracer::start($trace, $reporter, [
+            'qps' => [
+                'cache' => $cache,
+                'cacheItemClass' => CacheItem::class
+            ],
+            'startTime' => LARAVEL_START
+        ]);
+        RequestTracer::retroSpan(
+            microtime(true) - LARAVEL_START,
+            [
+                'name' => 'bootstrap'
+            ]
+        );
+
+        \Event::listen('Illuminate\Database\Events\QueryExecuted', function($event) {
             RequestTracer::retroSpan(
-                microtime(true) - LARAVEL_START,
+                $event->time * 0.001,
                 [
-                    'name' => 'bootstrap'
+                    'name' => $event->connectionName,
+                    'labels' => [
+                        'query' => $event->sql
+                    ]
                 ]
             );
-
-            \Event::listen('Illuminate\Database\Events\QueryExecuted', function($event) {
-                RequestTracer::retroSpan(
-                    $event->time * 0.001,
-                    [
-                        'name' => $event->connectionName,
-                        'labels' => [
-                            'query' => $event->sql
-                        ]
-                    ]
-                );
-            });
-        }
+        });
     }
 
     /**
@@ -58,5 +63,16 @@ class GoogleCloudProvider extends ServiceProvider
         $this->app->singleton(ServiceBuilder::class, function($app) {
             return new ServiceBuilder($app['config']['services']['google']);
         });
+        $this->app->singleton(TraceClient::class, function($app) {
+            return $app->make(ServiceBuilder::class)->trace();
+        });
+    }
+
+    public function provides()
+    {
+        return [
+            ServiceBuilder::class,
+            TraceClient::class
+        ];
     }
 }
