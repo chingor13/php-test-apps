7c7e1646903bf3cf223a3fa78caf4307068f78c8
Jeff Ching <chingor@google.com>
2017-03-30 10:08:11 -0700

    Happy path for default tracing

--- symfony/blog/src/Google/Symfony/GoogleBundle.php
+++ symfony/blog/src/Google/Symfony/GoogleBundle.php
@@ -4,7 +4,8 @@ namespace Google\Symfony;
 
 use Google\Cloud\ServiceBuilder;
 use Google\Cloud\Trace\RequestTracer;
-use Google\Cloud\Trace\Reporter\TraceReporter;
+use Google\Cloud\Trace\Reporter\SyncReporter;
+use Google\Cloud\Trace\Sampler\QpsSampler;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
@@ -13,21 +14,24 @@ class GoogleBundle extends Bundle
 
     public function boot()
     {
+        // don't trace if we're running in the console (i.e. a php artisan command)
+        if (php_sapi_name() == 'cli') {
+            return;
+        }
+
         if (!defined('SYMFONY_START')) {
             trigger_error('Please define \'SYMFONY_START\' in your `autoload.php`', E_USER_WARNING);
             define('SYMFONY_START', microtime(true));
         }
         $builder = new ServiceBuilder(['projectId' => 'chingor-php-gcs']);
         $trace = $builder->trace();
-        $reporter = new TraceReporter($trace);
-        RequestTracer::start($trace, $reporter, [
-            'enabled' => true,
-            'startTime' => SYMFONY_START
-        ]);
-        RequestTracer::retroSpan(
-            microtime(true) - SYMFONY_START,
-            ['name' => 'bootstrap']
-        );
+        $reporter = new SyncReporter($trace);
+
+        RequestTracer::start($reporter);
+
+        // create a span from the initial start time until now as 'bootstrap'
+        RequestTracer::startSpan(['name' => 'bootstrap', 'startTime' => SYMFONY_START]);
+        RequestTracer::finishSpan();
 
         $doctrine = $this->container->get('doctrine');
         $em = $doctrine->getConnection();
